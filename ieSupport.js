66342802684b8c55dec71b73237ea2ed9ca0a557
arinashirobokova <106390705+arinashirobokova@users.noreply.github.com>
2022-05-27 19:14:42 +0400

    Delete ieSupport.js

--- ieSupport.js
+++
@@ -1,8 +0,0 @@
-var errUserAgent = document.getElementById('errorUserAgent');
-errUserAgent.style.display = 'none';
-
-var usrAg = navigator.userAgent;
-if (usrAg == "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv 11.0) like Gecko") {
-	errUserAgent.style.display = 'block';
-	window.stop();
-}
